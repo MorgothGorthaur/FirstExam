60db284565b8edc66ce3a113132065fb48f56cd6
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-02-12 18:26:38 +0200

    added controller

---
+++ src/main/java/exam/controller/ExamRestController.java
@@ -0,0 +1,32 @@
+package exam.controller;
+
+import exam.dto.ManufacturerDto;
+import exam.dto.SouvenirDto;
+import exam.service.Exam;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/exam")
+@AllArgsConstructor
+public class ExamRestController {
+    private final Exam exam;
+
+    @GetMapping
+    public List<ManufacturerDto> getManufacturers() {
+        return exam.getManufactures();
+    }
+
+    @GetMapping
+    public List<SouvenirDto> getSouvenirs() {
+        return exam.getSouvenirs();
+    }
+
+    @GetMapping("/{id}")
+    public List<SouvenirDto> getManufacturersSouvenirs(@PathVariable Long id) {
+        return exam.getSouvenirsByManufacturerId(id);
+    }
+
+}
