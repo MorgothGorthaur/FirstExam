72423c3edca82842490d45ffb3222e9940a0b90a
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-02-12 20:00:12 +0200

    refactored

--- src/main/java/exam/model/Souvenir.java
+++ src/main/java/exam/model/Souvenir.java
@@ -1,5 +1,6 @@
 package exam.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.*;
 
@@ -13,6 +14,7 @@ import java.util.Objects;
 public class Souvenir {
     private Long id;
     private String name;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate date;
     private double price;
     private Manufacturer manufacturer;
