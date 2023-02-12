8b4de51de5f97162e0d96640849de5b93dd6db7d
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-02-12 14:06:10 +0200

    added ability of converting to model

--- src/main/java/exam/dto/ManufacturerDto.java
+++ src/main/java/exam/dto/ManufacturerDto.java
@@ -1,8 +1,16 @@
 package exam.dto;
 
+import exam.model.Manufacturer;
 import lombok.NonNull;
-public record ManufacturerDto(@NonNull Long id,@NonNull String name,@NonNull String country) {
-    public ManufacturerDto{
-        if(id < 0L && name.equals("") && country.equals("")) throw new RuntimeException();
+
+import java.util.ArrayList;
+
+public record ManufacturerDto(Long id, @NonNull String name, @NonNull String country) {
+    public ManufacturerDto {
+        if ((id != null && id < 0L) && name.equals("") && country.equals("")) throw new RuntimeException();
+    }
+
+    public Manufacturer toManufacturer() {
+        return new Manufacturer(id, name, country, new ArrayList<>());
     }
 }
--- src/main/java/exam/dto/SouvenirDto.java
+++ src/main/java/exam/dto/SouvenirDto.java
@@ -1,10 +1,16 @@
 package exam.dto;
 
+import exam.model.Souvenir;
 import lombok.NonNull;
 
 import java.time.LocalDate;
-public record SouvenirDto(@NonNull Long id,@NonNull String name, double price,@NonNull LocalDate date) {
-    public SouvenirDto{
-        if(id < 0L && name.equals("") && price < 0) throw new RuntimeException();
+
+public record SouvenirDto(Long id, @NonNull String name, double price, @NonNull LocalDate date) {
+    public SouvenirDto {
+        if ((id != null && id < 0L) && name.equals("") && price < 0) throw new RuntimeException();
+    }
+
+    public Souvenir toSouvenir() {
+        return new Souvenir(id, name, date, price, null);
     }
 }
