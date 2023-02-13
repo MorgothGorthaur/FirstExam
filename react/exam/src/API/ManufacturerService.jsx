ff0e305f68b1beee5e1645d53fe3b29df508e1b1
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-02-13 23:44:38 +0200

    added form

--- react/exam/src/API/ManufacturerService.jsx
+++ react/exam/src/API/ManufacturerService.jsx
@@ -7,4 +7,20 @@ export default class ManufacturerService {
             alert(e);
         }
     }
+
+    static async addManufacturer(name, country) {
+        try {
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(name, country)
+            }
+            const response = await fetch('http://localhost:8080/exam/manufacturers');
+            return await response.json();
+        } catch (e) {
+            alert(e);
+        }
+    }
 }
--- react/exam/src/Components/ManufacturerList.jsx
+++ react/exam/src/Components/ManufacturerList.jsx
@@ -1,9 +1,12 @@
 import React, {useState, useEffect} from "react";
 import Loader from '../UI/Loader/Loader';
 import ManufacturerService from "../API/ManufacturerService";
+import {Button, Modal} from "react-bootstrap";
+import ManufacturerForm from "./ManufacturerForm";
 const ManufacturerList = () => {
-    const [manufacturers, setManufacturers] = useState();
+    const [manufacturers, setManufacturers] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [modal, setModal] = useState(true);
     useEffect(() => {
         setLoading(true);
         setTimeout(() => {
@@ -16,18 +19,36 @@ const ManufacturerList = () => {
         setManufacturers(await ManufacturerService.getAll());
     }
 
+    const addManufacturer = (manufacturer) =>{
+        setManufacturers([...manufacturers, manufacturer]);
+        setModal(false);
+    }
     return (
         <div>
-            {
-                loading ? (
-                    <div>
-                        <Loader/>
-                    </div>
-                ) : (
-                    <div>
-                    </div>
-                )
-            }
+            {loading ? (
+                <div>
+                    <Loader/>
+                </div>
+            ) : (
+                <div>
+                    {manufacturers.length ? (
+                        <div>
+                            {manufacturers.map(manufacturer =>
+                                <div>
+                                    <h1> g </h1>
+                                </div>
+                            )
+                            }
+                        </div>
+                    ) : (
+                        <div>
+                            <h1> manufacturers not founded! </h1>
+                        </div>
+                    )}
+                    <Button onClick={() => setModal(true)}> add manufacturer</Button>
+                    <Modal show={modal} onHide={setModal}> <ManufacturerForm CreateOrUpdate={addManufacturer} /> </Modal>
+                </div>
+            )}
         </div>
     );
 };
