9766d174a29c3fe64feaaa31508cc0342213333d
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-02-13 02:03:43 +0200

    moved file name to properties

--- src/main/java/exam/dao/DaoImpl.java
+++ src/main/java/exam/dao/DaoImpl.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.SneakyThrows;
 import exam.model.Manufacturer;
-import exam.model.Souvenir;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -17,18 +17,18 @@ import java.util.Map;
 
 @Component
 public class DaoImpl implements Dao {
-    private final String MANUFACTURER_FILE_NAME = "manufacturers.json";
-    private final String SOUVENIRS_FILE_NAME = "souvenirs.json";
+    private final String FILE_NAME;
     private final ObjectMapper mapper;
 
-    public DaoImpl() {
+    public DaoImpl(@Value("${file.name}") String FILE_NAME) {
+        this.FILE_NAME = FILE_NAME;
         mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
     }
 
     @Override
     public Map<Long, Manufacturer> readManufacturers() {
-        try (var reader = new BufferedReader(new FileReader(MANUFACTURER_FILE_NAME))) {
+        try (var reader = new BufferedReader(new FileReader(FILE_NAME))) {
             var manufactures = new HashMap<Long, Manufacturer>();
             var line = "";
             while ((line = reader.readLine()) != null) {
@@ -46,7 +46,7 @@ public class DaoImpl implements Dao {
     @SneakyThrows
     @Override
     public void saveManufactures(Collection<Manufacturer> manufacturers) {
-        try (var writer = new BufferedWriter(new FileWriter(MANUFACTURER_FILE_NAME))) {
+        try (var writer = new BufferedWriter(new FileWriter(FILE_NAME))) {
             for (var manufacturer : manufacturers) writer.append(mapper.writeValueAsString(manufacturer)).append("\n");
         }
     }
