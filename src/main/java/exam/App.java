24a1c4af9b48f2c4ce0a1cf67db5363290590efe
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-02-14 18:24:37 +0200

    added buffered reader

--- src/main/java/exam/App.java
+++ src/main/java/exam/App.java
@@ -3,6 +3,7 @@ package exam;
 import exam.handler.ManufacturerHandler;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,16 +11,18 @@ import java.io.BufferedReader;
 
 
 @RequiredArgsConstructor
-public class App {
-    ManufacturerHandler manufacturerHandler;
-    BufferedReader reader;
+@SpringBootApplication
+public class App implements CommandLineRunner {
+    private final ManufacturerHandler manufacturerHandler;
+    private final BufferedReader reader;
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
-        new App().run();
     }
 
+
+    @Override
     @SneakyThrows
-    private void run() {
+    public void run(String... args) {
         System.out.println(reader.readLine());
     }
 }
