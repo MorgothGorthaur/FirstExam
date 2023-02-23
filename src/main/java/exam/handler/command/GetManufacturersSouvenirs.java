ecaa8908913563e6cd80731d5ca523b58050f08e
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-02-23 15:45:20 +0200

    added new command

---
+++ src/main/java/exam/handler/command/GetManufacturersSouvenirs.java
@@ -0,0 +1,31 @@
+package exam.handler.command;
+
+import exam.dto.mapper.Mapper;
+import exam.exception.ManufacturedNotFoundException;
+import exam.repository.Repository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+@RequiredArgsConstructor
+public class GetManufacturersSouvenirs implements Command{
+    private final Repository repository;
+    private final Mapper mapper;
+    @Override
+    public String getName() {
+        return "get_manufacturers_souvenirs";
+    }
+
+    @Override
+    public void printUsage() {
+        System.out.println(getName() + " \"manufacturer id\" - for getting souvenirs by manufacturer id");
+    }
+
+    @Override
+    public void execute(List<String> args) {
+        var id = Long.parseLong(args.get(0));
+        var manufacturer = repository.getManufacturerById(id).orElseThrow(() -> new ManufacturedNotFoundException(id));
+        manufacturer.getSouvenirs().stream().map(mapper::toSouvenirFullDto).forEach(System.out::println);
+    }
+}
