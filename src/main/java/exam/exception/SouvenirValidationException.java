9801bef5db656b898d28ac83268c292de0f6a006
MorgothGorthaur <victortarasov13032001@gmail.com>
2023-02-25 23:40:21 +0200

    refactored : rewrote message

--- src/main/java/exam/exception/SouvenirValidationException.java
+++ src/main/java/exam/exception/SouvenirValidationException.java
@@ -2,6 +2,6 @@ package exam.exception;
 
 public class SouvenirValidationException extends IllegalArgumentException {
     public SouvenirValidationException() {
-        super("date must be not after today`s date and price must be higher then 0");
+        super("date must be not after today`s date and price must be >= 0");
     }
 }
